4100e3177c332202acc9210a70e5154b8bfad44f
981389892ga <suporteweb36@gmail.com>
2024-10-11 14:27:20 -0300

    criando repositório teste

---
+++ script.js
@@ -0,0 +1,4 @@
+// Função simples para mostrar uma mensagem de boas-vindas
+document.addEventListener('DOMContentLoaded', function() {
+    alert('Seja bem-vindo à Primeira Igreja Batista - Cidade Operária!');
+});
